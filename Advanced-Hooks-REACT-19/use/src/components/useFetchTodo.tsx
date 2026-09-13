490e2924e8b7b28126bef6891cfcd0059bec9eed
Abdullah <abdullahbhtty01@gmail.com>
2026-09-14 00:06:32 +0500

    commit

--- Advanced-Hooks-REACT-19/use/src/components/useFetchTodo.tsx
+++ Advanced-Hooks-REACT-19/use/src/components/useFetchTodo.tsx
@@ -1,10 +1,9 @@
 import { use } from "react";
-
+const fetchData = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/todos/1");
+  return res.json();
+};
 const useFetchTodo = () => {
-  const fetchData = async () => {
-    const res = await fetch("https://jsonplaceholder.typicode.com/todos/1");
-    return res.json();
-  };
   const data = use(fetchData());
   return <div>{data.title}</div>;
 };
