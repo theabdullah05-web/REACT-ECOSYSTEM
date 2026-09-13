c8ce31843fe2470c9376ef1fab3beda918cedc3e
Abdullah <abdullahbhtty01@gmail.com>
2026-09-14 00:02:32 +0500

    commit

--- Advanced-Hooks-REACT-19/use/src/App.tsx
+++ Advanced-Hooks-REACT-19/use/src/App.tsx
@@ -1,11 +1,12 @@
 // import FetchTodo from "./components/FetchTodo";
+import { Suspense } from "react";
 import UseFetchTodo from "./components/useFetchTodo";
 
 const App = () => {
   return (
-    <>
+    <Suspense>
       <UseFetchTodo />
-    </>
+    </Suspense>
   );
 };
 
