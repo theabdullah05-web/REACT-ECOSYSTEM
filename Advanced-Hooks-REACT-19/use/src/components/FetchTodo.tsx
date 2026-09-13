db8ef7d865d58731eaa3ce67f28a7bd81df7c751
Abdullah <abdullahbhtty01@gmail.com>
2026-09-13 23:49:44 +0500

    commit

--- Advanced-Hooks-REACT-19/use/src/components/FetchTodo.tsx
+++ Advanced-Hooks-REACT-19/use/src/components/FetchTodo.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const FetchTodo = () => {
   interface data {
@@ -6,6 +6,18 @@ const FetchTodo = () => {
   }
   const [data, setData] = useState<data | null>(null);
   const [loading, setLoading] = useState<boolean>(true);
+  useEffect(()=>{
+    const fetchData=async()=>{
+      const API="https://jsonplaceholder.typicode.com/todos/1"
+    try{
+      const res=await fetch(API)
+      if(!result){
+        throw new Error("Network response was not ok")
+      }
+            const result=res.json()
+    }
+    }
+  },[])
   return <>Abdullah</>;
 };
 
