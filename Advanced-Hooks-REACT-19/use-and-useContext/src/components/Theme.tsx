93e4a5835cf5f4c205ec92e9c2f6a0ee86bd1056
Abdullah <abdullahbhtty01@gmail.com>
2026-09-14 16:08:25 +0500

    commit

---
+++ Advanced-Hooks-REACT-19/use-and-useContext/src/components/Theme.tsx
@@ -0,0 +1,11 @@
+import { createContext } from "react";
+
+type Theme = "light" | "dark";
+
+interface ThemeContextType {
+  theme: Theme;
+  toggleTheme: () => void;
+}
+export const themeContext = createContext<ThemeContextType | undefined>(
+  undefined,
+);
