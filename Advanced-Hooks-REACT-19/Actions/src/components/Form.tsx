365402d77bd3ec0816c817b1ce336db085bd94c3
Abdullah <abdullahbhtty01@gmail.com>
2026-09-14 16:41:34 +0500

    commit

--- Advanced-Hooks-REACT-19/Actions/src/components/Form.tsx
+++ Advanced-Hooks-REACT-19/Actions/src/components/Form.tsx
@@ -1,5 +1,11 @@
 const Form = () => {
-  return <div>Form</div>;
+  return (
+    <form>
+      <label htmlFor=""></label>
+      <label htmlFor=""></label>
+      <label htmlFor=""></label>
+    </form>
+  );
 };
 
 export default Form;
