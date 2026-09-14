9b85995acda19ff08b02c6dde4df25c53faaac47
Abdullah <abdullahbhtty01@gmail.com>
2026-09-14 17:27:02 +0500

    commit

--- Advanced-Hooks-REACT-19/Actions/src/components/Counter.tsx
+++ Advanced-Hooks-REACT-19/Actions/src/components/Counter.tsx
@@ -1,5 +1,18 @@
+import { useActionState } from "react";
+
 const Counter = () => {
-  return <h2></h2>;
+  const increment = async (previousState, formData) => {
+    console.log(formData.get("name"));
+    return previousState + 1;
+  };
+  const [state, formAction] = useActionState(increment, 0);
+  return (
+    <form>
+      <h2>{state}</h2>
+      <button formAction={formAction}>Increment</button>
+      <input name="name" />
+    </form>
+  );
 };
 
 export default Counter;
