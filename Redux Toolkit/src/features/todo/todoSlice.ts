12f911cc4597365f20145e71e0c8b53a4f5fca81
Abdullah <abdullahbhtty01@gmail.com>
2026-09-15 19:24:50 +0500

    commit

--- Redux Toolkit/src/features/todo/todoSlice.ts
+++ Redux Toolkit/src/features/todo/todoSlice.ts
@@ -42,6 +42,6 @@ export const todoSlice = createSlice({
 });
 
 // Action creators are generated for each case reducer function
-export const { addTodo, markAsDone } = todoSlice.actions;
+export const { addTodo, deleteTodo, markAsDone } = todoSlice.actions;
 
 export default todoSlice.reducer;
