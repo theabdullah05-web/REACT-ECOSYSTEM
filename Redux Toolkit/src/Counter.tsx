99d2b55e9c598f9068905c5806dd9d5ba526be8b
Abdullah <abdullahbhtty01@gmail.com>
2026-09-15 18:26:41 +0500

    commit

--- Redux Toolkit/src/Counter.tsx
+++ Redux Toolkit/src/Counter.tsx
@@ -1,15 +1,18 @@
 import { useDispatch, useSelector } from "react-redux";
 import type { RootState } from "./app/store";
 import { decrement, increment } from "./features/counter/counterSlice";
+import { useState } from "react";
 
 const Counter = () => {
   const count = useSelector((state: RootState) => state.counter.value);
   const dispatch = useDispatch();
+  const [state, setState] = useState<number | InputEvent>();
   return (
     <>
       <h2>{count}</h2>
       <button onClick={() => dispatch(increment())}>+</button>
       <button onClick={() => dispatch(decrement())}>-</button>
+      <input value={state} onChange={(e) => setState(e.target.value)} />
     </>
   );
 };
