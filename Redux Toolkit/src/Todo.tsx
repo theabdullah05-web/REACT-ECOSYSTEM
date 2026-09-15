89dcc80986513bede51700ee20afd79ddb530c4c
Abdullah <abdullahbhtty01@gmail.com>
2026-09-15 18:45:23 +0500

    commit

--- Redux Toolkit/src/Todo.tsx
+++ Redux Toolkit/src/Todo.tsx
@@ -1,5 +1,17 @@
+import { useSelector } from "react-redux";
+import type { RootState } from "./app/store";
+
 const Todo = () => {
-  return <></>;
+  const todo = useSelector((state: RootState) => state.todo.value);
+  return (
+    <>
+      <ul>
+        <li></li>
+        <li></li>
+        <li></li>
+      </ul>
+    </>
+  );
 };
 
 export default Todo;
