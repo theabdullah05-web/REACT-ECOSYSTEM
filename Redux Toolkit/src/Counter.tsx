76df1bd9aa1d934a6f4b71250e36053240b9ab9e
Abdullah <abdullahbhtty01@gmail.com>
2026-09-15 18:14:58 +0500

    commit

--- Redux Toolkit/src/Counter.tsx
+++ Redux Toolkit/src/Counter.tsx
@@ -1,4 +1,9 @@
+import { useDispatch, useSelector } from "react-redux";
+import type { RootState } from "./app/store";
+
 const Counter = () => {
+  const count = useSelector((state: RootState) => state.counter.value);
+  const dispatch = useDispatch();
   return <div>Counter</div>;
 };
 
