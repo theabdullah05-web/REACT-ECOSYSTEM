5f8d12586e183a551e08cda825fca0f12100c62a
Abdullah <abdullahbhtty01@gmail.com>
2026-09-15 17:48:36 +0500

    commit

--- Redux Toolkit/src/main.tsx
+++ Redux Toolkit/src/main.tsx
@@ -1,9 +1,20 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
+import { store } from "./app/store";
+import { Provider } from "react-redux";
+const container = document.getElementById("root");
 
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-);
+if (container) {
+  const root = createRoot(container);
+
+  root.render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+  );
+} else {
+  throw new Error(
+    "Root element with ID 'root' was not found in the document. Ensure there is a corresponding HTML element with the ID 'root' in your HTML file.",
+  );
+}
