d12d3d5db449be1f4eb16daa32c14f6c4fdd3341
Abdullah <abdullahbhtty01@gmail.com>
2026-09-16 20:22:37 +0500

    commit

--- RTK Query/src/components/AllData.tsx
+++ RTK Query/src/components/AllData.tsx
@@ -1,8 +1,12 @@
 import { useGetAllProductsQuery } from "../services/dummyData";
 
 const AllData = () => {
-  const [data, isLoading, error] = useGetAllProductsQuery();
-  return <div>AllData</div>;
+  const { data, isLoading, error } = useGetAllProductsQuery();
+
+  if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>Failed to load products.</div>;
+
+  return <div>{data ? JSON.stringify(data) : "No products found."}</div>;
 };
 
 export default AllData;
