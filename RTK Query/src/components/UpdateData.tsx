357363d1b4698de4c17637f3132eea8d68d51571
Abdullah <abdullahbhtty01@gmail.com>
2026-09-16 21:25:19 +0500

    commit

---
+++ RTK Query/src/components/UpdateData.tsx
@@ -0,0 +1,35 @@
+import { useUpdateProductMutation } from "../services/dummyData";
+
+const AddNewData = ({ id }) => {
+  const [addNewProduct, { data, isLoading, error }] =
+    useUpdateProductMutation();
+  const handleClick = async () => {
+    const updatedProduct = {
+      id: Math.floor(Math.random() * 1000) + 1,
+      title: "Product Updated",
+      description: "One of the best updated Products",
+    };
+    await addNewProduct({ id, updatedProduct });
+  };
+  if (error) {
+    return <h1>Some Error Occured</h1>;
+  } else if (isLoading) {
+    return <h1>Loading...</h1>;
+  }
+  return (
+    <>
+      {data ? (
+        <>
+          <p>{data.id}</p>
+          <p>{data.title}</p>
+          <p>{data.description}</p>
+        </>
+      ) : (
+        <></>
+      )}
+      <button onClick={handleClick}>Add Data</button>
+    </>
+  );
+};
+
+export default AddNewData;
