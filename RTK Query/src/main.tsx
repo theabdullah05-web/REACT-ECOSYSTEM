3331f3a20c4d5795cc11991acec15655ea35f0c1
Abdullah <abdullahbhtty01@gmail.com>
2026-09-16 19:48:38 +0500

    commit

--- RTK Query/src/main.tsx
+++ RTK Query/src/main.tsx
@@ -1,9 +1,20 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
+import App from "./App";
+import { store } from "./app/store";
+import { Provider } from "react-redux";
 
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-);
+const container = document.getElementById("root");
+
+if (container) {
+  const root = createRoot(container);
+
+  root.render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+  );
+} else {
+  throw new Error(
+    "Root element with ID 'root' was not found in the document. Ensure there is a corresponding HTML element with the ID 'root' in your HTML file.",
+  );
+}
