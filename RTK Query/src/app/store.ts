25a62e5179486400df38a345be05abbd49b1359d
Abdullah <abdullahbhtty01@gmail.com>
2026-09-16 20:12:23 +0500

    commit

--- RTK Query/src/app/store.ts
+++ RTK Query/src/app/store.ts
@@ -1,8 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { productApi } from "../services/dummyData";
+import { setupListeners } from "@reduxjs/toolkit/query";
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    [productApi.reducerPath]: productApi.reducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(productApi.middleware),
 });
-
+setupListeners(store.dispatch);
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
