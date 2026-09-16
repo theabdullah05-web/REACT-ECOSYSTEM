179b9425df2b692fa5f8e580692f7526c47771d9
Abdullah <abdullahbhtty01@gmail.com>
2026-09-16 21:56:37 +0500

    commit

--- RTK Query/src/components/DeleteProduct.tsx
+++ RTK Query/src/components/DeleteProduct.tsx
@@ -1,5 +1,16 @@
-const DeleteProduct = () => {
-  return <div>DeleteProduct</div>;
+import { useDeleteProductMutation } from "../services/dummyData";
+
+const DeleteProduct = ({ id: number }) => {
+  const [deleteProduct, { data, isLoading, error }] =
+    useDeleteProductMutation();
+  const handleClick = () => {
+    deleteProduct(id);
+  };
+  return (
+    <div>
+      <button onClick={handleClick}>Delete</button>
+    </div>
+  );
 };
 
 export default DeleteProduct;
