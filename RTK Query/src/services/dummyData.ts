54c8b452b684720439f947a9d648352736e38243
Abdullah <abdullahbhtty01@gmail.com>
2026-09-16 20:00:31 +0500

    commit

---
+++ RTK Query/src/services/dummyData.ts
@@ -0,0 +1,15 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export const dummyApi = createApi({
+  reducerPath: "products",
+  baseQuery: fetchBaseQuery({ baseUrl: "https://dummyjson.com" }),
+  endpoints: (builder) => ({
+    getAllProducts: builder.query<any, any>({
+      query: (name) => `pokemon/${name}`,
+    }),
+  }),
+});
+
+// Export hooks for usage in functional components, which are
+// auto-generated based on the defined endpoints
+export const { useGetAllProductsQuery } = dummyApi;
