1629088b1d384aeea288f5c67ea78d99b64012d3
Abdullah <abdullahbhtty01@gmail.com>
2026-09-16 20:38:13 +0500

    commit

--- RTK Query/src/components/SpecificProduct.tsx
+++ RTK Query/src/components/SpecificProduct.tsx
@@ -1,12 +1,25 @@
-import { useState } from "react";
+import { useState, type FormEvent } from "react";
 import { useGetProductByIdQuery } from "../services/dummyData";
 
 const SpecificProduct = () => {
   const [num, setNum] = useState<string>("");
   const { data } = useGetProductByIdQuery(parseInt(num));
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+    useGetProductByIdQuery(parseInt(num));
+  };
   return (
     <>
-      <input />
+      <form onSubmit={handleSubmit}>
+        <input value={num} onChange={(e) => setNum(e.target.value)} />
+        <button type="submit">Submit</button>
+        {data?.products.map((el) => (
+          <>
+            <h2 key={el.id}>{el.title}</h2>
+            <p>{el.description}</p>
+          </>
+        ))}
+      </form>
     </>
   );
 };
