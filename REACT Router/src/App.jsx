3b3686aa1a98a63b70c058f2892bb030edb47777
Abdullah <abdullahbhtty01@gmail.com>
2026-09-18 17:10:12 +0500

    commit

--- REACT Router/src/App.jsx
+++ REACT Router/src/App.jsx
@@ -1,13 +1,21 @@
 import { createBrowserRouter } from "react-router-dom";
 import Home from "./components/Home";
+import About from "./components/About";
+import Dashboard from "./components/Dashboard";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Home />,
   },
-  {},
-  {},
+  {
+    path: "/about",
+    element: <About />,
+  },
+  {
+    path: "/dashboard",
+    element: <Dashboard />,
+  },
 ]);
 const App = () => {
   return <div>App</div>;
