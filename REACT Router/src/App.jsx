34d76440aa528d1c0f9363dd017f6629d12fd32f
Abdullah <abdullahbhtty01@gmail.com>
2026-09-18 17:26:03 +0500

    commit

--- REACT Router/src/App.jsx
+++ REACT Router/src/App.jsx
@@ -3,7 +3,7 @@ import Home from "./components/Home";
 import About from "./components/About";
 import Dashboard from "./components/Dashboard";
 import Nav from "./components/Nav";
-
+import "./style.css";
 const router = createBrowserRouter([
   {
     path: "/",
