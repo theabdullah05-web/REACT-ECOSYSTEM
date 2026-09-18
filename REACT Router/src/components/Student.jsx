552f5a5325d525a5257bb8a6fd4344165a8a5de7
Abdullah <abdullahbhtty01@gmail.com>
2026-09-18 17:35:15 +0500

    commit

--- REACT Router/src/components/Student.jsx
+++ REACT Router/src/components/Student.jsx
@@ -1,5 +1,8 @@
+import { useParams } from "react-router-dom";
+
 const Student = () => {
-  return <div>Student</div>;
+  let { id } = useParams();
+  return <div>{id}</div>;
 };
 
 export default Student;
