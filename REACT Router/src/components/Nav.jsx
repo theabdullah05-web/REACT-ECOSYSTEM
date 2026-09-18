b4e85717452372840d705600acd66e8eb2c2e815
Abdullah <abdullahbhtty01@gmail.com>
2026-09-18 17:22:01 +0500

    commit

---
+++ REACT Router/src/components/Nav.jsx
@@ -0,0 +1,13 @@
+import { NavLink } from "react-router-dom";
+
+const Nav = () => {
+  return (
+    <>
+      <NavLink />
+      <NavLink />
+      <NavLink />
+    </>
+  );
+};
+
+export default Nav;
