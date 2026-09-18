d0443e71ec1c015520a02004f022585c22fa04a9
Abdullah <abdullahbhtty01@gmail.com>
2026-09-18 17:28:21 +0500

    commit

--- REACT Router/src/components/Nav.jsx
+++ REACT Router/src/components/Nav.jsx
@@ -3,18 +3,21 @@ import { NavLink } from "react-router-dom";
 const Nav = () => {
   return (
     <>
-      <NavLink to={"/"} className={(isActive) => (isActive ? "active" : "")}>
+      <NavLink
+        to={"/"}
+        className={({ isActive }) => (isActive ? "active" : "")}
+      >
         Home
       </NavLink>
       <NavLink
         to={"/about"}
-        className={(isActive) => (isActive ? "active" : "")}
+        className={({ isActive }) => (isActive ? "active" : "")}
       >
         About
       </NavLink>
       <NavLink
         to={"/dashboard"}
-        className={(isActive) => (isActive ? "active" : "")}
+        className={({ isActive }) => (isActive ? "active" : "")}
       >
         Dashboard
       </NavLink>
