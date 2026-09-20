0a957c82fe5770d8cf787615f9df9199380cba67
Abdullah <abdullahbhtty01@gmail.com>
2026-09-20 13:26:45 +0500

    commit

--- JWT/api/index.js
+++ JWT/api/index.js
@@ -1,6 +1,16 @@
 const express = require("express");
 const app = express();
 
+const users = [
+  {
+    id: 1,
+    username: "Abdullah",
+    password: "4256ab4256",
+    isAdmin: true,
+  },
+  { id: 1, username: "Abdullah", password: "4256ab4256", isAdmin: true },
+];
+
 app.listen(8080, () => {
   console.log("backend is working!!!");
 });
