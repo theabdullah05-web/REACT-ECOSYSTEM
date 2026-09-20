a86c90e4105916c2d2dd9163d4dc56178a638ecf
Abdullah <abdullahbhtty01@gmail.com>
2026-09-20 13:32:41 +0500

    commit

--- JWT/api/index.js
+++ JWT/api/index.js
@@ -11,6 +11,18 @@ const users = [
   { id: 2, username: "John Doe", password: "12345", isAdmin: false },
 ];
 
+app.post("/api/user", (req, res) => {
+  const { username, password } = req.body;
+  let user = users.find(
+    (el) => el.username === username && el.password === password,
+  );
+  if (user) {
+    res.json(user);
+  } else {
+    res.status(400).json("username or password is wrong");
+  }
+});
+
 app.listen(8080, () => {
   console.log("backend is working!!!");
 });
