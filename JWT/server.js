097e80202ada321a79c481a057641062be3a8521
Abdullah <abdullahbhtty01@gmail.com>
2026-09-21 16:00:58 +0500

    commit

--- JWT/server.js
+++ JWT/server.js
@@ -5,7 +5,20 @@ const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
 app.use(express.json());
 
-const users = [];
+const users = [
+  {
+    username: "Abdullah",
+    password: "$2b$10$IFmawNbmXDActxmgSyrM6uNAv8rJ08XPPy6X9umpoulWhDoqMVGOq",
+  },
+  {
+    username: "john",
+    password: "$2b$10$Tp3cCY3d.5dgIrvXdoUpX.HoOq5vQgMc.2wP22oJMHHuTYrdRNnjG",
+  },
+  {
+    username: "Doom",
+    password: "$2b$10$i7u2VfTwOpCJ.25DdPDbve5GsEeDuzkrSUB.N01wPyburLm1FM3LK",
+  },
+];
 
 const authenticateToken = (req, res, next) => {
   const authHeader = req.headers["authorization"];
