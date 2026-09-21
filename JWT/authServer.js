e6808e4fed74b70f50c5906e4636aadd5a8c25ff
Abdullah <abdullahbhtty01@gmail.com>
2026-09-21 15:25:10 +0500

    commit

--- JWT/authServer.js
+++ JWT/authServer.js
@@ -4,7 +4,7 @@ const app = express();
 const jwt = require("jsonwebtoken");
 app.use(express.json());
 
-let refreshToken = [];
+let refreshTokens = [];
 
 const generateAccessToken = (user) => {
   return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: "15s" });
@@ -13,7 +13,7 @@ const generateAccessToken = (user) => {
 app.post("/refreshToken", (req, res) => {
   const { token } = req.body;
   if (token === null) return res.sendStatus(401);
-  if (!refreshToken.includes(token)) return res.sendStatus(403);
+  if (!refreshTokens.includes(token)) return res.sendStatus(403);
   jwt.verify(token, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
     if (err) return res.sendStatus(403);
     const accessToken = generateAccessToken({ name: user.name });
@@ -28,6 +28,7 @@ app.post("/login", (req, res) => {
 
   const accessToken = generateAccessToken(user);
   const refreshToken = jwt.sign(user, process.env.REFRESH_TOKEN_SECRET);
+  refreshTokens.push(refreshToken);
   res.json({ accessToken: accessToken, refreshToken: refreshToken });
 });
 
