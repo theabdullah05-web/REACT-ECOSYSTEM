e4b9d8d01b1c37602dccf2947006427720ad7bef
Abdullah <abdullahbhtty01@gmail.com>
2026-09-21 14:42:15 +0500

    commit

---
+++ JWT/server2.js
@@ -0,0 +1,41 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const jwt = require("jsonwebtoken");
+app.use(express.json());
+
+const users = [
+  { username: "Abdullah", title: "Post 1" },
+  { username: "John Doe", title: "Post 2" },
+];
+
+const authenticateToken = (req, res, next) => {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+  if (token === null) {
+    return res.sendStatus(401);
+  }
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+    if (err) {
+      return res.sendStatus(403);
+    }
+    req.user = user;
+    next();
+  });
+};
+app.get("/posts", authenticateToken, (req, res) => {
+  res.json(users.filter((el) => el.username === req.user.name));
+});
+
+app.post("/login", (req, res) => {
+  //Authentication
+  const { username } = req.body;
+  const user = { name: username };
+
+  const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET);
+  res.json({ accessToken: accessToken });
+});
+
+app.listen(8080, () => {
+  console.log("app is listening on port 8080");
+});
