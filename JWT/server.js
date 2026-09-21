1e49cd2255c23a96ed3dfb8548383a0ee6f1b108
Abdullah <abdullahbhtty01@gmail.com>
2026-09-21 13:13:29 +0500

    commit

--- JWT/server.js
+++ JWT/server.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const app = express();
+const jwt = require("jsonwebtoken");
+app.use(express.json());
 
 const users = [
   { username: "Abdullah", title: "Post 1" },
