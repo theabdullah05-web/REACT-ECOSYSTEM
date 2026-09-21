3b9b591edc73b7ed0e08f693b377ba35a2fbde17
Abdullah <abdullahbhtty01@gmail.com>
2026-09-21 13:10:34 +0500

    commit

--- JWT/server.js
+++ JWT/server.js
@@ -1,5 +1,15 @@
 const express = require("express");
 const app = express();
+
+const users = [
+  { username: "Abdullah", title: "Post 1" },
+  { username: "John Doe", title: "Post 2" },
+];
+
+app.get("/", (req, res) => {
+  res.json;
+});
+
 app.listen(8080, () => {
   console.log("app is listening on port 8080");
 });
