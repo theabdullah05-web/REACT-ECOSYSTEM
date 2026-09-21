74319a63cae78a2039e13c06a598acfb9ed479b4
Abdullah <abdullahbhtty01@gmail.com>
2026-09-21 15:21:15 +0500

    commit

--- JWT/authServer.js
+++ JWT/authServer.js
@@ -13,7 +13,7 @@ const generateAccessToken = (user) => {
 app.post("/refreshToken", (req, res) => {
   const { token } = req.body;
   if (token === null) return res.sendStatus(401);
-  if (!refreshTokens.includes(token)) return res.sendStatus(403);
+  if (!refreshToken.includes(token)) return res.sendStatus(403);
   jwt.verify(token, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
     if (err) return res.sendStatus(403);
     const accessToken = generateAccessToken({ name: user.name });
